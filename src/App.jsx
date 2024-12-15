6bc8032c3865ac9953f18d170af920c50a1317e9
Asjad Ilahi <160521387+Asjad-Ilahi@users.noreply.github.com>
2024-12-15 15:06:04 +0500

    added sign-in page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AboutUs from './pages/aboutus/aboutUs';
 import { SignUpForm } from './pages/signup/signUp';
+import { LoginForm } from './pages/login/login';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<AboutUs />} />
         <Route path='/sign-up' element ={<SignUpForm/>} />
+        <Route path='/sign-in' element = {<LoginForm/>} />
       </Routes>
     </BrowserRouter>
   );
