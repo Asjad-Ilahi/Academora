1d2e1883ad53a00968e5bbaaa5e828fa27f422c1
Wahab-x78 <116077215+Wahab-x78@users.noreply.github.com>
2025-01-30 07:53:44 +0500

    Resolved issue of post and comment section

--- frontend/src/pages/explore/explore.jsx
+++ frontend/src/pages/explore/explore.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
-import MainContent from './components/mainContent/mainContent';
+import { useAuthStore } from '../../store/store.jsx';
 import Layout from '../../components/layout/layout';
+import MainContent from './components/mainContent/mainContent';
+import ForceLogin from '../../components/forceLogin/forcelogin.jsx'; // Import the new ForceLogin component
 
 function Explore() {
+  // const { user } = useAuthStore(); // Check if user is logged in
+
+  // if (!user) {
+  //   return <ForceLogin />; // Show login prompt if not authenticated
+  // }
+
   return (
     <Layout>
       <MainContent />
